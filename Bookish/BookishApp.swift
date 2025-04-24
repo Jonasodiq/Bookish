2866cae1c4083d5440b46c3656ff2899a109d937
Jonasodiq <jonasniyazson@hotmail.com>
2025-04-24 13:59:34 +0200

    Initial Commit

---
+++ Bookish/BookishApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookishApp.swift
+//  Bookish
+//
+//  Created by Jonas Niyazson on 2025-04-24.
+//
+
+import SwiftUI
+
+@main
+struct BookishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookish/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bookish
+//
+//  Created by Jonas Niyazson on 2025-04-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
