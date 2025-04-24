6a446aa3a66e2d6924748aa07c2700739afb0c8c
Jonasodiq <jonasniyazson@hotmail.com>
2025-04-24 18:11:23 +0200

    Added menu

--- Bookish/Views/ContentView.swift
+++ Bookish/Views/ContentView.swift
@@ -8,39 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    @EnvironmentObject var authViewModel: AuthViewModel
-
-    var body: some View {
-        TabView {
-            HomeGridView()
-                .tabItem {
-                    Image(systemName: "house")
-                    Text("Hem")
-                }
-
-            BookListView() // sök-vy från API
-                .tabItem {
-                    Image(systemName: "magnifyingglass")
-                    Text("Sök")
-                }
-
-            MyBooksView() // placeholder
-                .tabItem {
-                    Image(systemName: "book")
-                    Text("Mina böcker")
-                }
-
-            FavoritesView() // placeholder
-                .tabItem {
-                    Image(systemName: "star.fill")
-                    Text("Favoriter")
-                }
+  var body: some View {
+    TabView {
+      HomeGridView()
+        .tabItem {
+          Image(systemName: "house")
+          Text("Home")
         }
+      
+      BookListView()
+        .tabItem {
+          Image(systemName: "magnifyingglass")
+          Text("Search")
+        }
+      
+      MyBooksView()
+          .tabItem {
+              Image(systemName: "book")
+              Text("My Books")
+          }
+
+      FavoritesView()
+          .tabItem {
+              Image(systemName: "star.fill")
+              Text("Favorites")
+          }
+      }
     }
-}
-
-
-
-#Preview {
+  }
+  
+  #Preview {
     ContentView()
-}
+  }
+
+/**
+ 📖 New Books -> BookListView.swift   <-Lista från API
+ 📚 My Books ->   MyBooksView.swift   <-Sparade böcker från Firestore
+ ⏳ Last Read  -> LastReadView.swift   <-Senaste lästa boken
+ 📤 Share Book-> ShareBookView.swift <-Möjlighet att dela en bok
+ ⭐ Favorites ->   FavoritesView.swift    <-Visar favoriter (med filter)
+ */
