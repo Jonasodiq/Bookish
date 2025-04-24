433c3d1b537d2ebc8ea31814da70a0766514a7b4
Jonasodiq <jonasniyazson@hotmail.com>
2025-04-24 21:20:24 +0200

    Fix

---
+++ Bookish/Views/Books/FavoritesView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesView.swift
+//  Bookish
+//
+//  Created by Jonas Niyazson on 2025-04-24.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+      Text("⭐ Your favorite books")
+       .font(.title)
+       .padding()
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
